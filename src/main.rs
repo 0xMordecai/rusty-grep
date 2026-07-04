c0ef794fe64cc898b1d08e9093507be159f85060
0xMordecai <sir.lahrach.mohamed@gmail.com>
2026-07-04 13:50:10 +0100

    rusty-grep[Reading the Argument Values] cargo allows your minigrep program to read any command line arguments passed to it and then collect the values into a vector.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // allows your minigrep program to read any command line arguments passed to it and then collect the values into a vector.
+    let args: Vec<String> = env::args().collect();
+    dbg!(args);
 }
