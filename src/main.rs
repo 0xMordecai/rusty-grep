811079cc3476a4afcdf29d221a37d32a39cdb87b
0xMordecai <sir.lahrach.mohamed@gmail.com>
2026-07-04 16:37:13 +0100

    rusty-grep[Improving the Error Message-Returning a Result Instead of Calling panic] Working on it - Exiting with an error code if building a Config fails

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     // allows your minigrep program to read any command line arguments passed to it and then collect the values into a vector.
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Proplem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
