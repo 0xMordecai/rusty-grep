6c80bfd2d378e4335999dd1c6e31c41f2e217d9b
0xMordecai <sir.lahrach.mohamed@gmail.com>
2026-07-04 14:18:29 +0100

    rusty-grep[Reading a File] Done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     // allows your minigrep program to read any command line arguments passed to it and then collect the values into a vector.
@@ -10,4 +10,8 @@ fn main() {
 
     println!("Searching for {query}");
     println!("In file {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text \n{contents}");
 }
