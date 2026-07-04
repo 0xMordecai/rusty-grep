5f6e84f334cdb2d2aa70ee893f717a60fec6c210
0xMordecai <sir.lahrach.mohamed@gmail.com>
2026-07-04 14:00:48 +0100

    rusty-grep[aving the Argument Values in Variables] Done

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,11 @@ use std::env;
 fn main() {
     // allows your minigrep program to read any command line arguments passed to it and then collect the values into a vector.
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    // dbg!(args);
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
 }
